959b7aa41e485d8a25dfda40853bb95990ae11ab
vietlott <vietlottvl@gmail.com>
2017-08-08 00:05:01 +0700

    checkout

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+	
+  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+  validates :name, :address, :email, :pay_type, :presence => true
+validates :pay_type, :inclusion => PAYMENT_TYPES
 end
